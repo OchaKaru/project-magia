64c8d92f5408fba10efa46acce28927f351982b3
Kalvin Garcia <school.kalvin.g@gmail.com>
2023-07-03 00:55:43 +0000

    made push.sh executable

--- push.sh
+++ push.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+git config --global user.email "school.kalvin.g@gmail.com"
+git config --global user.name "Kalvin Garcia"
+
+git add .
+git commit
+
 eval "$(ssh-agent -s)"
 ssh-add ./sessions/ssh/kalvin_git_key
-git 
+
+git push
